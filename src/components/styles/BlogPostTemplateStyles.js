35b6a8e094312ff9cdcb05bac22b30bd69a3c616
Austin Mallar <austinmallar@gmail.com>
2019-07-27 11:53:05 -0700

    Added single blog post style

---
+++ src/components/styles/BlogPostTemplateStyles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+const BlogPostTemplateStyles = styled.div`
+  ul.post-nav {
+    list-style-type: none;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 5rem;
+  }
+
+  li.next-post,
+  li.prev-post {
+  }
+`
+
+export default BlogPostTemplateStyles
